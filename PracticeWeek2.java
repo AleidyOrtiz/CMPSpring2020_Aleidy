d2e10f131fc60e914b484e238f71fc7f1c3b5b9d
AleidyOrtiz <60441119+AleidyOrtiz@users.noreply.github.com>
2020-02-03 12:44:26 -0500

    pRACTIVE WEEK#2

--- PracticeWeek2.java
+++ PracticeWeek2.java
@@ -1,4 +1,26 @@
-
+/**
+ * 
+ * @Aleidy Ortiz
+ * created: 2/3/2020
+ *description: Variable Practice
+ **/
 public class PracticeWeek2 {
-
-}
+	public static void main(String[] args) {
+ //Defining Variables
+	double car= 10.35;
+	byte phone = 5;
+	int pencil= 1000;
+	
+	//Declaring Variables 
+     String name1;
+	 String name2;
+	 String name3;
+	
+	//Assigning Values 
+	name1 = "Angel";
+	name2= "Maria";
+	name3= "Kylie";
+	
+	//printing
+	System.out.println(name1);
+	}}
