6bd8d2eb00d83b6d6f73e62fad686f3b4afcae96
AleidyOrtiz <60441119+AleidyOrtiz@users.noreply.github.com>
2020-02-26 12:44:59 -0500

    Add files via upload

---
+++ PrintsNumbers.java
@@ -0,0 +1,16 @@
+package week2part2;
+
+/**
+ * 
+ * @author Aleidy
+ *
+ */
+
+import java.util.Scanner;
+public class PrintsNumbers {
+public static void main(String[] args) {
+	Scanner keyboard=new Scanner(System.in);
+	for (int i=5; i<100; i++)
+		System.out.println(i);
+}
+}
